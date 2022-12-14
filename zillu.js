3db9dc7620b770f60e21e7db0503f4e95732fed8
zilan9119 <120160655+zilan9119@users.noreply.github.com>
2022-12-14 10:34:27 +0530

    Add files via upload

---
+++ zillu.js
@@ -0,0 +1,255 @@
+<!Doctype html>
+<head>
+    <title>registraion</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/jilan.css" rel="stylesheet">
+    <script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+  <img src="https://wallpapercave.com/wp/wp7339905.jpg"height="200px" width="150px">
+    <center><h1>oneplus</h1></center>
+    <h2 id="head">REGISTRATION FORM</h2>
+    <hr>
+    <div class="container" id="con1">
+        <h3 id="pd">Personal Details</h3>
+        <div class="row" id="row1">
+            <div class="row" id="roww">
+                <div class="col">
+                  <input type="text" class="form-control" placeholder="First name" aria-label="First name">
+                </div>
+                <div class="col">
+                  <input type="text" class="form-control" placeholder="Middle name" aria-label="Middle name">
+                </div>
+                <div class="col">
+                    <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
+                  </div>
+              </div>
+        </div>       
+    <div class="row" id="row2">
+        <h5>GENDER</h5>
+        <div class="col" id="gcol1">
+        <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+            <label class="form-check-label" for="inlineRadio1">Male</label>
+          </div>
+        </div>
+        <div class="col" id="gcol2">
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+            <label class="form-check-label" for="inlineRadio2">Female</label>
+          </div>
+        </div>
+        <div class="col" id="gcol3">
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option3">
+            <label class="form-check-label" for="inlineRadio2">Others</label>
+          </div>
+        </div>
+    </div>
+    <div class="row" id="rowd">
+        <div class="col">
+        <label class="row" for="rowd"><h5>Date of Birth</h5></label>
+        <input type="date">
+    </div>
+    </div>
+    <div class="row" id="rowf">
+        <div class="col">
+            <label class="row" for="rowf"><h5>Father name</h5></label>
+            <input type="text"  placeholder="Father name" aria-label="Father name">
+          </div>
+    </div>
+    <hr>
+    <div class="row" id="ci">
+        <label class="row" for="ci"><h5>Contact Info.</h5></label>
+        <div class="col"id=ph>
+            <input type="number"  placeholder="Phone number" aria-label="Phone Number">
+        </div>
+        <div class="col"id=eid>
+            <input type="email"  placeholder="E-mail" aria-label="E-mail">
+        </div>
+    </div>
+    <hr>
+    <div class="row" id="rowadd">
+        <label for="exampleInputEmail1" class="form-label" ><h5>Address</h5></label>
+        <div class="col">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="HNO./Flat no./building" aria-label="HNO./Flat no./building">
+              </div>
+        </div>
+        <div class="col">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="Street name/Area name" aria-label="Street name/Area name">
+              </div>
+        </div>
+    </div>
+    <div class="row" id="row7">
+        <div class="col"><form>
+            <div class="col">
+                <input type="text" class="form-control" placeholder="city/Town/Village" aria-label="city/Town/Village">
+              </div>
+            </div>
+        <div class="col">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="District" aria-label="District">
+              </div>
+        </div>
+    </div>
+    <div class="row" id="row8">
+        <div class="col">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="State" aria-label="State">
+              </div>
+         </div>
+        <div class="col">
+            <div class="col">
+                <input type="text" class="form-control" placeholder="PINcode" aria-label="PINcode">
+              </div>
+        </div>
+    </div>
+    <hr>
+    <div class="row" id="idp">
+      <div class="col">
+        <label class="row" for="rowf"><h5>ID Proof</h5></label>
+        <input type="text"  placeholder="ID type" aria-label="ID type">
+      </div>
+    </div>
+        <div class="row" id="idup">
+            <input type="file">
+        </div>
+        <div class="col" id="pic">
+          <p><b>uoload Photo</b></p>
+          <input type="file">
+          <div class="row">
+            <p><b>Upload Signature</b></p>
+            <input type="file">
+          </div>
+        </div>
+        <hr>
+ 
+  <div>
+    <h3 id="ed">Education Qualification</h3>
+  </div>
+  
+    <div class="row" id="row9">
+        
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Qualification" aria-label="Qualification">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Institution" aria-label="Institution">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Percentage" aria-label="Percentage">
+        </div>
+        <div class="col">
+            <input type="file">
+        </div>
+    </div>
+    <div class="row" id="row10">
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Qualification" aria-label="Qualification">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Institution" aria-label="Institution">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Percentage" aria-label="Percentage">
+        </div>
+        <div class="col">
+            <input type="file">
+        </div>
+    </div>
+    <div class="row" id="row11">
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Qualification" aria-label="Qualification">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Institution" aria-label="Institution">
+        </div>
+        <div class="col">
+            <input type="text" class="form-control" placeholder="Percentage" aria-label="Percentage">
+        </div>
+        <div class="col">
+            <input type="file">
+        </div>        
+</div>
+<div class="row" id="row12">
+  <div class="col">
+      <input type="text" class="form-control" placeholder="Qualification" aria-label="Qualification">
+  </div>
+  <div class="col">
+      <input type="text" class="form-control" placeholder="Institution" aria-label="Institution">
+  </div>
+  <div class="col">
+      <input type="text" class="form-control" placeholder="Percentage" aria-label="Percentage">
+  </div>
+  <div class="col">
+      <input type="file">
+  </div>  
+</div>
+<div class="row" id="resup">
+  <p><b>Upload Resume</b></p>
+  <br>
+ 
+  <input type="file">
+</div>
+<hr>
+<div class="row" id="submit">
+  <div class="col">
+   <center> <button type="button" class="btn btn-info">Submit</button> </center>
+  </div>
+</div>
+  </div>
+  <div class="container" id="con2">
+    <div class="info">
+        <p id="name"><b></b>  </p>
+            <P id="mail"><b>MAIL ID :</b>  </P>
+             <P id="phno"><b>PHONE :</b>  </p>
+        </div>
+        <h3 id="edq"><u>EDUCUTAION QUALIFICATIONS</U></H3>
+        <div class="tab" id="edtab">
+        <table >
+        <tr>
+        <th>Qualification </th>
+        <th>University/Board</th>
+        <th>  Percentage/CGPA</th>
+        <th>Marks Memo</th>
+        </tr>
+        <tr>
+        <td id="hed">Degree B.Sc (MSCs)</td>
+        <td id="ins1">Rayalaseema University</td>
+        <td id="per1">7.12</td>
+        <td id="memo1"></td>
+        </tr>
+        <tr>
+        <td id="ed2">Intermediate (MPC)</td>
+        <td id="ins2">Board of Intermediate </td>
+        <td id="per2">9.44</td>
+        <td id="memo2"></td>
+        </tr>
+        <tr>
+        <td id="ed3">SSC</td>
+        <td id="ins3">Board of Secondary Education</td>
+        <td id="per3">10.0</td>
+        <td id="memo3"></td>
+        </tr>
+        <tr>
+            <td id="ed4">SSC</td>
+            <td id="ins4">Board of Secondary Education</td>
+            <td id="per4">9.7</td>
+            <td id="memo4"></td>
+            </tr>
+        </table>
+        </div>
+        <h3 style="color:brown"><u>PERSONAL DETAILS</u></h3>
+        <div class="pd">
+        <p id="fname"> Father Name   :</p>
+        <p id="dob"> Date of Birth :</p>
+        </p>
+        </div>
+        <p id="sign"><b>SIGNATURE</B></p>
+        </div>  
+  </div>
+</body>
+</html>
